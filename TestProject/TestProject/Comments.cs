58b46a2e17982f05abc7d154232f58295810b535
Mario Sosa <jsosa@southwestuniversity.edu>
2021-02-19 19:06:32 -0700

    First assignment

---
+++ TestProject/TestProject/Comments.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProject
+{
+    internal class Comments
+    {
+        internal int CommentId;
+
+        public string TheComment { get; internal set; }
+    }
+}
---
+++ TestProject/TestProject/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProject
+{
+    internal class User
+    {
+        public string Email { get; internal set; }
+        public string Password { get; internal set; }
+        public int UserId { get; internal set; }
+        public string UserName { get; internal set; }
+    }
+}
